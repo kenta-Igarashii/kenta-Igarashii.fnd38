80610b782859ac14ad064ad50639e1814ea098ac
kenta-Igarashii <diggy201@gmail.com>
2025-07-31 11:51:21 +0900

    fix

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 'use strict'
 
-// const yamate = ["東京", "神田", "秋葉原", "御徒町", "上野",
-//   "鶯谷", "日暮里", "西日暮里", "田端", "駒込", "巣鴨", "大塚",
-//   "池袋", "目白", "高田馬場", "新大久保", "新宿", "代々木", "原宿",
-//   "渋谷", "恵比寿", "目黒", "五反田", "大崎", "品川", "高輪ゲートウェイ",
-//   "田町", "浜松町", "新橋", "有楽町",];
-
-const yamate = ["東京", "神田", "秋葉原",];
+const yamate = ["東京", "神田", "秋葉原", "御徒町", "上野",
+  "鶯谷", "日暮里", "西日暮里", "田端", "駒込", "巣鴨", "大塚",
+  "池袋", "目白", "高田馬場", "新大久保", "新宿", "代々木", "原宿",
+  "渋谷", "恵比寿", "目黒", "五反田", "大崎", "品川", "高輪ゲートウェイ",
+  "田町", "浜松町", "新橋", "有楽町",];
 
 const dragDrop = () => {
   const resultDiv = document.getElementById("route-map");
@@ -107,6 +105,7 @@ const gameStart = () => {
   audioPlay(music);
   document.getElementsByClassName("start-btn")[0].style.display = "none";
   document.querySelector(".mute-btn").style.display = "block";
+  document.querySelector(".description").style.display = "block";
 };
 
 const restart = () => {
